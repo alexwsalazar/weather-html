00da9ae2663e64783c58f4db25d827e037ce1162
Alex Salazar <alexwsalazar@gmail.com>
2022-03-18 19:29:05 -0500

    add a readme layout

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# Weather app
+
+This is a weather app that allows you to search any city and will give you the current weather, date, and time
+
+
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## API Reference
+
+#### Get all items
+
+```http
+  GET /api/items
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `api_key` | `string` | **Required**. Your API key |
+
+#### Get item
+
+```http
+  GET /api/items/${id}
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to fetch |
+
+#### add(num1, num2)
+
+Takes two numbers and returns the sum.
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherinempeterson.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var form = document.querySelector(".top-banner form");
 var input = document.querySelector(".top-banner input");
 var msg = document.querySelector(".top-banner .msg");
 var list = document.querySelector(".ajax-section .cities");
+var list2 = document.querySelector(".ajax-section2 .cities");
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -14,7 +15,8 @@ form.addEventListener("submit", e => {
   
     //ajax here
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-  
+
+    var url2 =`https://api.openweathermap.org/data/2.5/forecast?lat=${inputVal}&appid=${apiKey}&units=metric`;
 
     ///// fetch to request data from api
 
@@ -56,6 +58,46 @@ form.addEventListener("submit", e => {
     form.reset();
     input.focus();
   });
+
+
+  // fetch(url2)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       var { main, name, sys, weather } = data;
+  //       var icon = `https://openweathermap.org/img/wn/${
+  //         weather[0]["icon"]
+  //       }@2x.png`;
+
+       
+
+  //       var li = document.createElement("li");
+  //       li.classList.add("city");
+
+        // create weather card here 
+    //     var markup = 
+    //     `<h2 class="city-name data-name="${name},${sys.country}">
+    //         <span>${name}</span>
+    //         <sup>${sys.country}</sup>
+    //       </h2>
+    //       <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+    //       <figure class="card-body">
+    //       <p class="date">${today}</p>
+    //         <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+    //         <figcaption class="description">${weather[0]["description"]}</figcaption>
+    //       </figure>`
+    //     li.innerHTML = markup;
+    //     list2.appendChild(li);
+    //   })
+    //   .catch(() => {
+    //     msg.textContent = "Please search for a valid city!!!!";
+        
+    //   });
+  
+    // msg.textContent = "";
+    // form.reset();
+    // input.focus();
+  
+
 
 
 
